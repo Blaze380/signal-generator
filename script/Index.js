6e741f43c1693fcbae5a298f586f6ef0bc7989ff
Blaze380 <144695514+Blaze380@users.noreply.github.com>
2023-09-12 19:53:06 +0300

    ghch

--- script/Index.js
+++ script/Index.js
@@ -5,12 +5,11 @@ var botaoLogin = document.getElementById("login_btn");
 
 botaoLogin.addEventListener("mouseenter",verificarCredenciais);
 botaoLogin.addEventListener("touchstart",verificarCredenciais);
-botaoLogin.addEventListener("click",verificarCredenciais);
+botaoLogin.addEventListener("focus",verificarCredenciais);
 function verificarCredenciais(){
      if((usuario.value === "blaze") && (senha.value === "creator") ){
          location.href = "Sinais.html";
      }else{
-        alert("Login")
          alert("Nome de usuário e/ou Senha incorrectos! \nTry it again!")
      }
 }
