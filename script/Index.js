1e7866b29496e81072b64eab729806f3cfe56b03
Blaze380 <144695514+Blaze380@users.noreply.github.com>
2023-09-12 20:47:55 +0300

    Submit button change

--- script/Index.js
+++ script/Index.js
@@ -1,15 +1,23 @@
 var usuario = document.getElementById("user");
 var senha = document.getElementById("password");
 var botaoLogin = document.getElementById("login_btn");
-
+async function delay(){
+    await new Promise(resolve => setTimeout(resolve,8000))
+    alert("sd")
+}
 
 botaoLogin.addEventListener("mouseenter",verificarCredenciais);
 botaoLogin.addEventListener("touchstart",verificarCredenciais);
-botaoLogin.addEventListener("focus",verificarCredenciais);
+botaoLogin.addEventListener("volumechange",()=>{
+    navigator.vibrate(200);
+    alert("Blaze é o criador!!!!!");
+});
 function verificarCredenciais(){
      if((usuario.value === "blaze") && (senha.value === "creator") ){
+        alert("Seja Bem vindo!")
          location.href = "Sinais.html";
      }else{
          alert("Nome de usuário e/ou Senha incorrectos! \nTry it again!")
      }
 }
+
